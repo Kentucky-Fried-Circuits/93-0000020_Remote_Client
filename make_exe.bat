09bb2d6020f85511e150307e6b9dfb4123acd0ad
Boris Barreto <bbarreto@solarstik.com>
2023-02-22 11:33:24 -0500

    Added selection for products: 24VDC PRO-Verter 5000-120 AR-4034 (20-0104034)

--- make_exe.bat
+++ make_exe.bat
@@ -1,1 +1,3 @@
 pyinstaller -F 93-0000020.py --windowed --hidden-import C:\Users\balano\Documents\GitHub\python-can-j1939\venv2\Lib\site-packages\can\interfaces\pcan\basic.py
+
+pyinstaller -F --add-data "C:\Users\YOUR_USER\Documents\GitHub\python-can-j1939;j1939" 93-0000020.py --windowed
